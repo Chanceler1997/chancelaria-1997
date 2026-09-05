02b1026ef2937b648b751323a06bf1e056572b11
Chanceler1997 <chancelaria1997@gmail.com>
2026-09-04 21:44:58 -0400

    Versão 6.1.1

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE='chancelaria-1997-v6.1.0';
+const CACHE='chancelaria-1997-v6.1.1';
 const CORE=[
-  './index.html?v=6.1.0','./index.html','./imprimir.html','./manifest.json',
+  './index.html?v=6.1.1','./index.html','./imprimir.html','./manifest.json',
   './icon-192.png','./icon-512.png','./cabecalho-oficial.jpg','./assinatura-chanceler.png','./certificado-base.png'
 ];
 self.addEventListener('install',event=>{
